6288cbbf74ecfd36cceea5746c59da8762a97b83
rezeroliu <1432409554@qq.com>
2024-09-27 12:39:42 +0000

    Deploying to gh-pages from @ rezeroliu/rezeroliu.github.io@de1c27aac90e6d7e4d6fc32d20803018ac403c1d 🚀

---
+++ assets/index.html-BKGp95s6.js
@@ -0,0 +1,9 @@
+import{_ as e,c as t,b as o,o as n}from"./app-Cr9wRBna.js";const a={};function r(i,c){return n(),t("div",null,[o(` ---
+title: 主要功能与配置演示
+index: false
+icon: laptop-code
+category:
+  - 使用指南
+---
+
+<Catalog /> `)])}const l=e(a,[["render",r],["__file","index.html.vue"]]),m=JSON.parse('{"path":"/demo/","title":"","lang":"zh-CN","frontmatter":{"head":[["meta",{"property":"og:url","content":"https://rezeroliu.github.io/demo/"}],["meta",{"property":"og:site_name","content":"旺仔珍知棒"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-09-27T12:05:29.000Z"}],["meta",{"property":"article:author","content":"Rezero.Liu"}],["meta",{"property":"article:modified_time","content":"2024-09-27T12:05:29.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-09-27T12:05:29.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Rezero.Liu\\",\\"url\\":\\"https://rezeroliu.github.io\\"}]}"]]},"headers":[],"git":{"createdTime":1721827043000,"updatedTime":1727438729000,"contributors":[{"name":"LuckyGoGo","email":"7643936+luckygogo@user.noreply.gitee.com","commits":3}]},"readingTime":{"minutes":0.07,"words":22},"filePathRelative":"demo/README.md","localizedDate":"2024年7月24日","excerpt":"<!-- ---\\ntitle: 主要功能与配置演示\\nindex: false\\nicon: laptop-code\\ncategory:\\n  - 使用指南\\n---\\n\\n<Catalog /> -->\\n"}');export{l as comp,m as data};
